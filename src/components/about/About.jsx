262295b532ff6f46d183444407172bb3231064e3
Helder Lagos <helderlagospro@gmail.com>
2023-02-08 15:27:52 -0300

    curriculo atualizado fevereiro 2023

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets//about.jpg";
-import CV from "../../assets//HelderLagos.pdf";
+import CV from "../../assets//Curriculo Helder Lagos.pdf";
 import Info from "./Info";
 
 const About = () => {
